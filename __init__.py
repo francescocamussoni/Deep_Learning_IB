4d64cde7eab49bf8785883d428d86a34c5256f3b
francescocamussoni <50533010+francescocamussoni@users.noreply.github.com>
2021-03-13 11:32:02 -0300

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-date: 26-09-2020
-File: ej7.py
-Author : Francesco Camussoni
-Email: camussonif@gmail.com francesco.camussoni@ib.edu.ar
-GitHub: https://github.com/francescocamussoni
-GitLab: https://gitlab.com/francescocamussoni
-Description:
-"""
